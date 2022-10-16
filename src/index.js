f1c27b6385ac606e38abb340dca082652a22a2ae
Richmond Blankson <mensahrichmondb@gmail.com>
2022-10-16 13:44:02 +0000

    learned how to build resuable, configurable and custom components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import faker from 'faker';
+import './style/App.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// custom components
+import Comment from './components/comment/Comment';
+import ApprovalCard from './components/approval/ApprovalCard';
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+
+function App(){
+  return (
+    <div className='ui container comments'>
+      <ApprovalCard>
+        <div>
+          <h4>Warning</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment 
+          author='Blankson' 
+          content='This world is awesome' 
+          timeAgo='Today at 6:00PM' 
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment 
+          author='Roses' 
+          content='Everything made was beautiful' 
+          timeAgo='Today at 9:30AM' 
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <Comment 
+          author='Mel' 
+          content='God is a designer' 
+          timeAgo='Today at 12:00PM' 
+          avatar={faker.image.image()}
+        />
+      </ApprovalCard>
+    </div>
+  );
+}
+
+root.render(<App />);
+
